205fc1fc01fd3ec905418c0868fa4c20acda5c70
Arnaud Gathy <Arnaud.gathy@gmail.com>
2018-09-07 19:12:14 +0200

    Removed auth from remote

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,6 +1,6 @@
 import {client} from '../index'
 
 export const auth = {
-  token: 'NDg2NjA4NDc0NjI1NTQwMTA2.DnBpQg.O4BWjNRswq142lwIVH999C86suU',
+  token: 'NDg2NjA4NDc0NjI1NTQwMTA2.DnRCbQ.4qjcmPp0NEjd9EAkHgNto7PMIAM',
   login: () => client.login(auth.token)
 }
