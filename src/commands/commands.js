94f3e941f75fb20c96fed7ef05dc35a7fea0a1b7
Arnaud Gathy <Arnaud.gathy@gmail.com>
2018-09-09 18:54:56 +0200

    added platform

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,5 +1,5 @@
 import info from '../../package.json'
-import {test} from '../index'
+import {type} from "os";
 
 export const commands = {
   help: {
@@ -23,7 +23,7 @@ export const commands = {
           info.version
         }\nAuteur: ${info.author}\nGithub: ${
           info.homepage
-        }\nRunning in ${process.env.NODE_ENV} environment`,
+        }\nRunning in ${process.env.NODE_ENV} environment on ${type()}`,
         {code: 'markdown'}
       ),
     info: 'Informations sur le bot',
