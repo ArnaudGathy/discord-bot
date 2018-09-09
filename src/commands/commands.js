795efa82673a58391c37803b4c8efbb30722071e
Arnaud Gathy <Arnaud.gathy@gmail.com>
2018-09-09 16:10:11 +0200

    Added NODE_ENV

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,30 +1,36 @@
 import info from '../../package.json'
 
-const infoMessage = `Nom: ${info.name}\nDescription: ${info.description}\nVersion : ${info.version}\nAuteur: ${info.author}\nGithub: ${info.homepage}\nRunning in : ${process.env.NODE_ENV || 'development'}`
+const infoMessage = `Nom: ${info.name}\nDescription: ${
+  info.description
+}\nVersion : ${info.version}\nAuteur: ${info.author}\nGithub: ${
+  info.homepage
+}\nRunning in : ${process.env.NODE_ENV}`
 
 export const commands = {
   help: {
     action: ({msg}) => msg.channel.send(commandList, {code: 'markdown'}),
-    info: "Affiche la liste des commandes",
+    info: 'Affiche la liste des commandes',
   },
   ping: {
     action: ({msg}) => msg.channel.send('pong'),
-    info: "Test la présence du bot avec un ping -> pong",
+    info: 'Test la présence du bot avec un ping -> pong',
   },
   info: {
     action: ({msg}) => msg.channel.send(infoMessage, {code: 'markdown'}),
-    info: "Informations sur le bot",
+    info: 'Informations sur le bot',
   },
   hello: {
     action: ({msg}) => msg.channel.send(`Kikoo ${msg.author} ( ͡° ͜ʖ ͡°)`),
-    info: "Dites bonjour au bot !",
+    info: 'Dites bonjour au bot !',
   },
 }
 
-const commandList = `# Commandes disponibles\n${Object.entries(commands).map(([key, value]) => `* !${key} - ${value.info}`).join("\n")}`
+const commandList = `# Commandes disponibles\n${Object.entries(commands)
+  .map(([key, value]) => `* !${key} - ${value.info}`)
+  .join('\n')}`
 
 export const fallback = async ({msg, command}) => {
   const channel = await msg.author.createDM()
-  const sadGif = "https://giphy.com/gifs/walk-away-sad-6bWNHVAoj7tNS"
+  const sadGif = 'https://giphy.com/gifs/walk-away-sad-6bWNHVAoj7tNS'
   channel.send(`Pas de commande associée à **${command}**\n${sadGif}`)
 }
