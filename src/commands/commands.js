32be448349a32e33df7f7c9561d8c25b3fe8ea3e
Arnaud <arno.firefox@gmail.com>
2020-10-25 10:59:42 +0100

    added roll command

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -2,11 +2,13 @@ import info from '../../package.json'
 import {type} from "os";
 import { getRandomGif } from '../utils/giphy';
 
+const commandsToIgnore = ['mjroll']
+
 export const commands = {
   help: {
     action: ({msg}) =>
       msg.channel.send(
-        `# Commandes disponibles\n${Object.entries(commands)
+        `# Commandes disponibles\n${Object.entries(commands.filter(command => !command['mjroll']))
           .map(([key, value]) => `* !${key} - ${value.info}`)
           .join('\n')}`,
         {code: 'markdown'}
@@ -35,11 +37,19 @@ export const commands = {
   },
   gif: {
     action: async ({msg, params}) => msg.channel.send(await getRandomGif(params)),
-    info: 'Renvoie un gif random associé au mot clé unique fourni (!gif cry)'
+    info: 'Renvoie un gif random associé au mot clé unique fourni (!gif <mot_clé>)'
   },
   random: {
     action: ({msg}) => msg.reply(`ton random : ${Math.floor((Math.random() * 100) + 1)}`),
     info: 'Renvoie un random entre 1 et 100'
+  },
+  roll: {
+    action: ({msg, params}) => {
+      const roll = Math.floor(Math.round((Math.random() * 100) + 1) / 5)
+      const message = params ? `${msg.author}: ${roll} + ${params} = ${Math.round(roll + params)}` : `${msg.author}: ${roll}`
+      msg.channel.send(message)
+    },
+    infi: 'Renvoie un jet de dé 20 associé à une statistique (/roll <statistique>)'
   }
 }
 
